9006ae794e9a11e7a0fac506352b1689cf8bc697
JohnHill5609 <hillj5609@gmail.com>
2020-10-23 12:56:27 +0800

    add py

---
+++ modefied.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+import yaml
+import random
+
+def up_yml(ip_server):
+        content1 = open('/home/runner/work/oktetodev/oktetodev/baota.yml', 'r', encoding="utf-8")
+        file_data1 = content1.read()
+        content1.close()
+        all_data = yaml.load_all(file_data1)
+        file1 = open('/home/runner/work/oktetodev/oktetodev/baota.yml', 'w', encoding='utf-8')
+
+        for data in all_data:
+            num = random.randint(100, 10000);
+            file1.write("---\n")
+            data['metadata']['labels']['testc'] = 'host{}'.format(num)
+            yaml.dump(data, file1)
+            print('成功')
+        file1.close()
+
+if __name__ == '__main__':
+    up_yml(ip_server='0.0.0.0')
